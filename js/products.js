0950f314a81eef29a37fa29135da1750d80b74a2
RocioPallas <rocio.pallasm@gmail.com>
2023-08-31 18:19:29 -0300

    Tarea 2 y 3, luego de revisión grupal

--- js/products.js
+++ js/products.js
@@ -1,5 +1,13 @@
-const autosURL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const catID = localStorage.getItem('catID');
+const productsUrl = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
 const productContainer = document.getElementById("productContainer");
+const ORDER_ASC = "asc";
+const ORDER_DESC = "desc";
+const ORDER_BY_PROD_REL = "Rel.";
+let currentProductsArray = [];
+let currentSortCriteria = "asc";
+let minPrice = undefined;
+let maxPrice = undefined;
 
 function productCard(p) {
     const card = document.createElement("div");
@@ -24,13 +32,98 @@ function productCard(p) {
     return card;
 }
 
-getJSONData(autosURL)
-    .then(function (response) {
-        if (response.status === "ok") {
-            let products = response.data.products;
-            products.forEach(element => {
-                let pCard = productCard(element);
-                productContainer.appendChild(pCard);
-            });
-        }
-    });
+
+function sortProducts(array){
+    let result = [];
+    if (currentSortCriteria === ORDER_ASC)
+    {
+        result = array.sort(function(a, b) {
+            if ( a.cost < b.cost ){ return -1; }
+            if ( a.cost > b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (currentSortCriteria === ORDER_DESC){
+        result = array.sort(function(a, b) {
+            if ( a.cost > b.cost ){ return -1; }
+            if ( a.cost < b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (currentSortCriteria === ORDER_BY_PROD_REL){
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.soldCount);
+            let bCount = parseInt(b.soldCount);
+
+            if ( aCount > bCount ){ return -1; }
+            if ( aCount < bCount ){ return 1; }
+            return 0;
+        });
+    }
+    
+    return result;
+}
+
+function showProductsList(arrayproduct){
+  productContainer.innerHTML = "";
+  arrayproduct.forEach(element => {
+    let pCard = productCard(element);
+    productContainer.appendChild(pCard);
+});
+}
+
+function sortAndShowProducts(criteria, array) {
+  currentSortCriteria = criteria;
+
+
+  showProductsList(sortProducts(array));
+}
+
+function filterAndShowProducts() {
+  let filteredArray = currentProductsArray.filter(product => {
+      if ((minPrice === undefined || minPrice=== "" || parseInt(minPrice) <= product.cost) &&
+          (maxPrice === undefined || maxPrice=== "" || parseInt(maxPrice) >= product.cost)) {
+          return true;
+      }
+      return false;
+  });
+
+  sortAndShowProducts(currentSortCriteria, filteredArray);
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(productsUrl)
+  .then(function (response) {
+      if (response.status === "ok") {
+          currentProductsArray = response.data.products;
+          showProductsList(currentProductsArray);
+      }
+  });
+
+  document.getElementById("filterPrice").addEventListener("click", function () {
+      minPrice = document.getElementById("rangeFilterPriceMin").value;
+      maxPrice = document.getElementById("rangeFilterPriceMax").value;
+      filterAndShowProducts();
+  });
+
+  document.getElementById("clearRangeFilter").addEventListener("click", function () {
+      document.getElementById("rangeFilterPriceMin").value = "";
+      document.getElementById("rangeFilterPriceMax").value = "";
+
+      minPrice = undefined;
+      maxPrice = undefined;
+
+      filterAndShowProducts();
+  });
+
+  document.getElementById("sortAsc").addEventListener("click", function () {
+    sortAndShowProducts(ORDER_ASC, currentProductsArray);
+  });
+
+  document.getElementById("sortDesc").addEventListener("click", function () {
+      sortAndShowProducts(ORDER_DESC, currentProductsArray);
+  });
+
+  document.getElementById("sortByCount").addEventListener("click", function () {
+      sortAndShowProducts(ORDER_BY_PROD_REL, currentProductsArray);
+  });
+
+});
