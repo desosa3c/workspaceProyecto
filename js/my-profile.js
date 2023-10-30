bc9befd7a06d171cb9af03e9342330a93178c156
gonzalo0101 <123899063+gonzalo0101@users.noreply.github.com>
2023-10-30 20:32:48 -0300

    Marca en rojo campos vacíos my-profile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,21 +1,25 @@
 document.getElementById("botonGuardar").addEventListener("click", function() {
-    var nombre = document.getElementById("nombre").value;
-    var apellido = document.getElementById("apellido").value;
-    var email = document.getElementById("email").value;
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const email = document.getElementById("email").value;
     
     if (nombre === "") {
       document.getElementById("nombre").style.borderColor = "red";
+      document.getElementById("nombre-error").style.display = "block";
     } else {
       document.getElementById("nombre").style.borderColor = "";
+      document.getElementById("nombre-error").style.display = "none";
     }
     
     if (apellido === "") {
       document.getElementById("apellido").style.borderColor = "red";
+      document.getElementById("apellido-error").style.display = "block";
     } else {
       document.getElementById("apellido").style.borderColor = "";
+      document.getElementById("apellido-error").style.display = "none";
     }
     
-    if (email === "") {
+    if (email === "" || /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
       document.getElementById("email").style.borderColor = "red";
     } else {
       document.getElementById("email").style.borderColor = "";
