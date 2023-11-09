9da3060195bffe3cf074f285d6c1d03032af3b70
Jacob Rodriguez <jerd1900@gmail.com>
2023-11-09 10:14:53 -0300

    correccion de guardado de datos

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,5 +1,5 @@
 document.getElementById("botonGuardar").addEventListener("click", function() {
-const nombre = document.getElementById("nombre").value;
+    const nombre = document.getElementById("nombre").value;
     const apellido = document.getElementById("apellido").value;
     const email = document.getElementById("email").value;
     
@@ -16,7 +16,7 @@ const nombre = document.getElementById("nombre").value;
       document.getElementById("apellido").style.borderColor = "";
       document.getElementById("apellido-error").style.display = "none";
     }
-  })
+  });
     
  
 
@@ -61,17 +61,35 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("telefono").value = formData.telefono;
   }
   
+  const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const email = document.getElementById("email").value;
+    
+    if (nombre === "") {
+      document.getElementById("nombre").style.borderColor = "red";
+      document.getElementById("nombre-error").style.display = "block";
+    } else {
+      document.getElementById("nombre").style.borderColor = "";
+      document.getElementById("nombre-error").style.display = "none";
+    }if (apellido === "") {
+      document.getElementById("apellido").style.borderColor = "red";
+      document.getElementById("apellido-error").style.display = "block";
+    } else {
+      document.getElementById("apellido").style.borderColor = "";
+      document.getElementById("apellido-error").style.display = "none";
+    }
+
   let img_defaut = "https://i.pinimg.com/736x/e5/91/dc/e591dc82326cc4c86578e3eeecced792.jpg";
   let file = document.getElementById("foto");
   let img = document.getElementById("img");
 
-  if(localStorage.getItem("foto")){
+  if(localStorage.getItem("foto")){ // consulta si ya existe una img en el almacenamiento local
     img_defaut = localStorage.getItem("foto");
     img.src = img_defaut;
   }else{
     img.src = img_defaut;
   }
-  file.addEventListener("change", e=>{
+  file.addEventListener("change", e=>{ // evento de cambio de img
     if(e.target.files[0]){
       const reader = new FileReader();
       reader.onload = function(e){
